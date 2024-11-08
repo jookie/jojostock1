e8edebc91f6e3b7f69ed5647eacfd01ea2e7a04d
jookie <dovpeles@gmail.com>
2024-11-08 15:02:48 -0500

    refactore

--- Stock_Market_prediction.py
+++ Stock_Market_prediction.py
@@ -14,172 +14,79 @@ from lib.rl.config import (
       TRAINED_MODEL_DIR,
       TENSORBOARD_LOG_DIR,
       DATA_FRAME_DIR,
-      INDICATORS,
       RESULTS_DIR,
 )
 from lib.rl.meta.preprocessor.yahoodownloader import YahooDownloader
 from lib.rl.meta.preprocessor.preprocessors import FeatureEngineer, data_split
 from lib.rl.meta.env_stock_trading.env_stocktrading import StockTradingEnv
 from lib.rl.agents.stablebaselines3.models import DRLAgent
-from stable_baselines3.common.logger import configure
 from lib.rl.plot import backtest_stats, backtest_plot, get_daily_return, get_baseline
 from lib.rl.main import check_and_make_directories
 from lib.rl.config_tickers import index_dict
-import os
-import itertools
 import matplotlib.pyplot as plt
 import warnings ; warnings.filterwarnings("ignore")
 # from pages.StockMarketApp import styles ; styles.set()
-from lib.utility.inputs import get_full_path, GetTickerList, set_yahoo_data_frame
+from lib.utility.inputs import get_full_path, GetTickerList, set_yahoo_data_frame, predict_with_models
 
-custom_css = """
-<style>
-body {
-background-color: black; /* Background color (black) */
-font-family: "Times New Roman", Times, serif; /* Font family (Times New Roman) */
-color: white; /* Text color (white) */
-line-height: 1.6; /* Line height for readability */
-}
-
-h1 {
-color: #3498db; /* Heading color (light blue) */
-}
-
-h2 {
-color: #e74c3c; /* Subheading color (red) */
-}
-
-p {
-margin: 10px 0; /* Margin for paragraphs */
-}
-
-</style>
-"""
-# Apply the custom CSS
-st.markdown(custom_css, unsafe_allow_html=True)
-
-def main(ticker_list, wf):
+def main(ticker_list, _wf):
   import pandas as pd
-  procDataFrame, processed_full, train, trade, mvo_df, stock_dimension, state_space = set_yahoo_data_frame(ticker_list, wf)
   
-  buy_cost_list = sell_cost_list = [0.001] * stock_dimension
-  num_stock_shares = [0] * stock_dimension
-  env_kwargs = {
-      "hmax": 100,
-      "initial_amount": 1000000,
-      "num_stock_shares": num_stock_shares,
-      "buy_cost_pct": buy_cost_list,
-      "sell_cost_pct": sell_cost_list,
-      "state_space": state_space,
-      "stock_dim": stock_dimension,
-      "tech_indicator_list": INDICATORS,
-      "action_space": stock_dimension,
-      "reward_scaling": 1e-4
-  }
-  e_train_gym = StockTradingEnv(df = train, **env_kwargs)
-  env_train, _ = e_train_gym.get_sb_env()
+  mvo_df, env_kwargs, trade, processed_full, models = set_yahoo_data_frame(ticker_list, _wf)
   
-  # st.write(type(env_train))
-  
-  agent = DRLAgent(env = env_train)
-  if_using_a2c = True
-  if_using_ddpg = True
-  if_using_ppo = True
-  if_using_td3 = True
-  if_using_sac = True
+  def get_e_trade_gym_results():
+    data_risk_indicator = processed_full[(processed_full.date<wf.train_end_date) & (processed_full.date>= wf.train_start_date)]
+    insample_risk_indicator = data_risk_indicator.drop_duplicates(subset=['date'])
+    
+    st.write(f"Vix Indicator: {insample_risk_indicator.vix.quantile(0.996)}")
+    st.write(insample_risk_indicator.vix.describe())
+    st.write(insample_risk_indicator.turbulence.describe())
+    st.write(insample_risk_indicator.turbulence.quantile(0.996))
 
-  def predict_with_models(models, environment):
-      """
-      Perform predictions using multiple trained models in the specified environment.
+    e_trade_gym = StockTradingEnv(df = trade, turbulence_threshold =70,  risk_indicator_col='vix', **env_kwargs)
+    env_trade, obs_trade = e_trade_gym.get_sb_env()
 
-      Parameters:
-      - models: A dictionary of trained models with names as keys.
-      - environment: The trading environment to be used for predictions.
+    st.write(trade.head())
+    st.write(trade.tail())
+    
+    results = predict_with_models(models, e_trade_gym)
+    
+    df_account_value_a2c = results["a2c"]["account_value"]
+    df_actions_a2c = results["a2c"]["actions"]
 
-      Returns:
-      - results: A dictionary containing DataFrames of account values and actions for each model.
-      """
-      results = {}
+    df_account_value_ddpg = results["ddpg"]["account_value"]
+    df_actions_ddpg = results["ddpg"]["actions"]
 
-      for model_name, trained_model in models.items():
-          df_account_value, df_actions = DRLAgent.DRL_prediction(
-              model=trained_model,
-              environment=environment
-          )
-          results[model_name] = {
-              "account_value": df_account_value,
-              "actions": df_actions
-          }
-      return results
+    df_account_value_ppo = results["ppo"]["account_value"]
+    df_actions_ppo = results["ppo"]["actions"]
+
+    df_account_value_td3 = results["td3"]["account_value"]
+    df_actions_td3 = results["td3"]["actions"]
+
+    df_account_value_sac = results["sac"]["account_value"]
+    df_actions_sac = results["sac"]["actions"]    
+
+    st.write(df_account_value_a2c.shape)
+    st.write(df_account_value_a2c.head())
+    st.write(df_account_value_a2c.tail())
+    
+    
+    df_result_a2c = df_account_value_a2c.set_index(df_account_value_a2c.columns[0])
+    df_result_ddpg = df_account_value_ddpg.set_index(df_account_value_ddpg.columns[0])
+    df_result_td3 = df_account_value_td3.set_index(df_account_value_td3.columns[0])
+    df_result_ppo = df_account_value_ppo.set_index(df_account_value_ppo.columns[0])
+    df_result_sac = df_account_value_sac.set_index(df_account_value_sac.columns[0])
       
-  def train_agent(agent, model_name = "a2c", total_timesteps=50000):
-      """
-      Train a model with the provided agent and model_name and total_timesteps 
-      """
-      # Get the model for A2C if applicable
-      __cached__model_ = agent.get_model(model_name)
-      # Set up logger
-      _tmp_path = RESULTS_DIR + '/' + model_name
-      _new_logger = configure(_tmp_path, ["stdout", "csv", "tensorboard"])
-      # Set the new logger
-      __cached__model_.set_logger(_new_logger)
-          
-      # Train the model
-      _trained = agent.train_model(
-      model=__cached__model_, 
-      tb_log_name=model_name,
-      total_timesteps=total_timesteps
-      )
-      return _trained
-
-  trained_a2c = train_agent(agent, "a2c", 5) if if_using_a2c else None
-  trained_ddpg = train_agent(agent, "ddpg", 5) if if_using_ddpg else None
-  trained_ppo = train_agent(agent, "ppo", 5) if if_using_ppo else None  
-  trained_td3 = train_agent(agent, "td3", 5) if if_using_td3 else None
-  trained_sac = train_agent(agent, "sac", 5) if if_using_sac else None
+    result = pd.merge(df_result_a2c, df_result_ddpg, left_index=True, right_index=True, suffixes=('_a2c', '_ddpg'))
+    result = pd.merge(result, df_result_td3, left_index=True, right_index=True, suffixes=('', '_td3'))
+    result = pd.merge(result, df_result_ppo, left_index=True, right_index=True, suffixes=('', '_ppo'))
+    result = pd.merge(result, df_result_sac, left_index=True, right_index=True, suffixes=('', '_sac'))  
+    
+    df_account_value_a2c.to_csv( get_full_path( "df_account_value_a2c.csv")) 
+    return result, df_account_value_a2c
+    
   
-  data_risk_indicator = processed_full[(processed_full.date<wf.train_end_date) & (processed_full.date>= wf.train_start_date)]
-  insample_risk_indicator = data_risk_indicator.drop_duplicates(subset=['date'])
+  result, df_account_value_a2c = get_e_trade_gym_results()
   
-  st.write(f"Vix Indicator: {insample_risk_indicator.vix.quantile(0.996)}")
-  st.write(insample_risk_indicator.vix.describe())
-  st.write(insample_risk_indicator.turbulence.describe())
-  st.write(insample_risk_indicator.turbulence.quantile(0.996))
-
-  e_trade_gym = StockTradingEnv(df = trade, turbulence_threshold =70,  risk_indicator_col='vix', **env_kwargs)
-  env_trade, obs_trade = e_trade_gym.get_sb_env()
-
-  st.write(trade.head())
-  st.write(trade.tail())
-  # Example usage:
-  models = {
-      "a2c": trained_a2c,
-      "ddpg": trained_ddpg,
-      "ppo": trained_ppo,
-      "td3": trained_td3,
-      "sac": trained_sac
-  }
-  results = predict_with_models(models, e_trade_gym)
-  # Access results for each model
-  df_account_value_a2c = results["a2c"]["account_value"]
-  df_actions_a2c = results["a2c"]["actions"]
-
-  df_account_value_ddpg = results["ddpg"]["account_value"]
-  df_actions_ddpg = results["ddpg"]["actions"]
-
-  df_account_value_ppo = results["ppo"]["account_value"]
-  df_actions_ppo = results["ppo"]["actions"]
-
-  df_account_value_td3 = results["td3"]["account_value"]
-  df_actions_td3 = results["td3"]["actions"]
-
-  df_account_value_sac = results["sac"]["account_value"]
-  df_actions_sac = results["sac"]["actions"]
-
-  st.write(df_account_value_a2c.shape)
-  st.write(df_account_value_a2c.head())
-  st.write(df_account_value_a2c.tail())
-
   fst = mvo_df
   fst = fst.iloc[0*29:0*29+29, :]
   tic = fst['tic'].tolist()
@@ -302,7 +209,9 @@ def main(ticker_list, wf):
   StockData = mvo.head(mvo.shape[0]-336)
   st.write(StockData)
   TradeData = mvo.tail(336)
-  st.write(procDataFrame.head())
+  
+  # st.write(procDataFrame.head())
+  
   TradeData.to_numpy()
 
   #compute asset returns
@@ -336,13 +245,13 @@ def main(ticker_list, wf):
   MVO_result = pd.DataFrame(Portfolio_Assets, columns=["Mean Var"])
 #   MVO_result
 
-  df_result_a2c = df_account_value_a2c.set_index(df_account_value_a2c.columns[0])
-  df_result_ddpg = df_account_value_ddpg.set_index(df_account_value_ddpg.columns[0])
-  df_result_td3 = df_account_value_td3.set_index(df_account_value_td3.columns[0])
-  df_result_ppo = df_account_value_ppo.set_index(df_account_value_ppo.columns[0])
-  df_result_sac = df_account_value_sac.set_index(df_account_value_sac.columns[0])
+  # df_result_a2c = df_account_value_a2c.set_index(df_account_value_a2c.columns[0])
+  # df_result_ddpg = df_account_value_ddpg.set_index(df_account_value_ddpg.columns[0])
+  # df_result_td3 = df_account_value_td3.set_index(df_account_value_td3.columns[0])
+  # df_result_ppo = df_account_value_ppo.set_index(df_account_value_ppo.columns[0])
+  # df_result_sac = df_account_value_sac.set_index(df_account_value_sac.columns[0])
   
-  df_account_value_a2c.to_csv( get_full_path( "df_account_value_a2c.csv"))
+  # df_account_value_a2c.to_csv( get_full_path( "df_account_value_a2c.csv"))
   #baseline stats
   st.write("==============Get Baseline Stats===========")
   df_dji_ = get_baseline(
@@ -358,10 +267,6 @@ def main(ticker_list, wf):
   df_dji.to_csv(get_full_path("df_dji+.csv"))
 
 
-  result = pd.merge(df_result_a2c, df_result_ddpg, left_index=True, right_index=True, suffixes=('_a2c', '_ddpg'))
-  result = pd.merge(result, df_result_td3, left_index=True, right_index=True, suffixes=('', '_td3'))
-  result = pd.merge(result, df_result_ppo, left_index=True, right_index=True, suffixes=('', '_ppo'))
-  result = pd.merge(result, df_result_sac, left_index=True, right_index=True, suffixes=('', '_sac'))
   result = pd.merge(result, MVO_result, left_index=True, right_index=True, suffixes=('', '_mvo'))
   result = pd.merge(result, df_dji, left_index=True, right_index=True, suffixes=('', '_dji'))
   result.columns = ['a2c', 'ddpg', 'td3', 'ppo', 'sac', 'mean var', 'dji']
@@ -375,10 +280,9 @@ def main(ticker_list, wf):
   st.pyplot(fig)
 
 if __name__ == "__main__":
-  from lib.utility.inputs import WorkflowScheduler
+  from lib.utility.inputs import WorkflowScheduler, setFirstPageTitle
   
-  st.title(" 📈 Stock Price Ptediction Training")
-  st.markdown("Trainig to predict stock price movements for a given stock ticker symbol and its foundamental ratios")
+  setFirstPageTitle()
   check_and_make_directories( [DATA_SAVE_DIR, TRAINED_MODEL_DIR, TENSORBOARD_LOG_DIR,    RESULTS_DIR, DATA_FRAME_DIR] )
   ticker_list = GetTickerList()
   wf = WorkflowScheduler()
