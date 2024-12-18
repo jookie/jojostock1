4436c48e17386ec8979024865f75a50c491184de
jookie <dovpeles@gmail.com>
2024-12-17 19:23:08 -0500

    refactore

--- pub.sh
+++ pub.sh
@@ -6,6 +6,14 @@
 # git remote add origin https://github.com/jookie/jojoStock.git
 # git push --set-upstream origin main
 # git branch -M main
+
+# git clone https://github.com/jookie/jojostock1.git
+# cd jojostock1
+# git checkout main
+# git pull origin main
+# git checkout -b dov_trade
+# git push -u origin dov_trade
+
 git add .
 git commit -m "refactore"
 # git push -u origin dov-refactoring
