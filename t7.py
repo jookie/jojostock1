9df1e6d437497531a4f9f802a859a818c827f6c1
jookie <dovpeles@gmail.com>
2024-12-21 20:20:56 -0500

    refactore

--- t7.py
+++ t7.py
@@ -1,15 +1,15 @@
 # run_backtest.py
 import math
 import sys
-sys.path.append('/Users/dovpeles/dov/jojostock1/lib/MLTradingBot')
+# sys.path.append('/Users/dovpeles/dov/jojostock1/lib/MLTradingBot')
 
-from datetime import datetime , timedelta 
+from datetime import datetime 
 import streamlit as st
 from lib.MLTradingBot.lumibot.backtesting.yahoo_backtesting import  YahooDataBacktesting
-
+from lib.MLTradingBot.lumibot.example_strategies.stock_bracket import StockBracket
 if __name__ == "__main__":
-    # from stock_momentum import Momentum
-    from lib.MLTradingBot.lumibot.example_strategies.stock_bracket import StockBracket
+    # Import Momentum
+    
     # Backtest this strategy
     backtesting_start = datetime(2023, 1, 1)
     backtesting_end = datetime(2024, 8, 1)
