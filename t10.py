3179f8cce08bd9fa95693f9efa473cb93f295a33
jookie <dovpeles@gmail.com>
2024-12-24 17:19:12 -0500

    refactore

--- t10.py
+++ t10.py
@@ -7,39 +7,40 @@ from lumibot.traders import Trader
 from lumibot.credentials import IS_BACKTESTING
 from lumibot.backtesting.yahoo_backtesting import  YahooDataBacktesting
 from lumibot.example_strategies.stock_sentiment import StockSentiment
+
+ALPACA_CREDS = {
+    "API_KEY": "PKXQGLU5DJJ30MUWS2G6", 
+    "API_SECRET": "vPSm9TeqjD7WhYYcuhhvdyXZiFjJQDSlO5ic5s1d", 
+    "PAPER": True
+}
 """
 Strategy Description
 This strategy will buy a few symbols that have 2x or 3x returns (have leverage), but will 
 also diversify and rebalance the portfolio often.
 """
-if __name__ == "__main__":
-    is_live = False
-    if is_live:
-        ####
-        # Run the strategy live
-        ####
-        from lumibot.credentials import ALPACA_CREDS
-        trader = Trader()
-        broker = Alpaca(ALPACA_CREDS)
-        strategy = StockSentiment(broker=broker)
-        trader.add_strategy(strategy)
-        trader.run_all()
-    else:
-        backtesting_start = datetime(2023, 6, 1)
-        backtesting_end = datetime(2023, 7, 31)
-        # 0.01% trading/slippage fee
-        trading_fee = TradingFee(percent_fee=0.005)
-        # Initialize the backtesting object
-        print("Starting Backtest...")
-        st.write(f"Starting Backtest...")
-        result = StockSentiment.backtest(
-            YahooDataBacktesting,
-            backtesting_start,
-            backtesting_end,
-            benchmark_asset="SPY",
-            parameters={},
-            buy_trading_fees=[trading_fee],
-            sell_trading_fees=[trading_fee],
-        )
-        print("Backtest result: ", result)
-        st.write(f"Backtest result: {result}")
+
+start_date  = datetime(2020,1,1)
+end_date    = datetime(2020,11,12) 
+# end_date    = datetime(2023,12,31) 
+broker      = Alpaca(ALPACA_CREDS) 
+strategy    = StockSentiment(name='mlstrat', broker=broker, 
+                    parameters={"symbol":"SPY", 
+                                "cash_at_risk":.5})
+strategy.backtest(
+    YahooDataBacktesting, 
+    start_date, 
+    end_date, 
+    parameters={"symbol":"SPY", "cash_at_risk":.5}
+)
+trader = Trader()
+trader.add_strategy(strategy)
+trader.run_all()
+# strategy = StockSentiment(broker=broker)
+ 
+        # result = StockSentiment.backtest(
+        #     YahooDataBacktesting,
+        #     backtesting_start,
+        #     backtesting_end,
+        #  )
+        # print("Backtest result: ", result)
+        # st.write(f"Backtest result: {result}")
