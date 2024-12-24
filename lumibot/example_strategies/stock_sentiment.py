806b969e1d0d8b28b8ed6f0a9abee336c36f0d51
jookie <dovpeles@gmail.com>
2024-12-24 15:24:58 -0500

    refactore

--- lumibot/example_strategies/stock_sentiment.py
+++ lumibot/example_strategies/stock_sentiment.py
@@ -1,52 +1,9 @@
-# Integrate Streamlit for user interaction with Lumibot logic while addressing threading issues like ValueError: signal only works in main thread,I  use subprocesses to separate the Streamlit frontend from the backend logic of Lumibot and pass a symbol and place an order with alpaca and lumibot. 
-from __future__ import annotations
-import streamlit as st
-# import warnings ; warnings.filterwarnings("ignore")
-BASE_URL = "https://paper-api.alpaca.markets"
-API_KEY = "PKXQGLU5DJJ30MUWS2G6"
-API_SECRET ="vPSm9TeqjD7WhYYcuhhvdyXZiFjJQDSlO5ic5s1d"
-api_secret = API_SECRET
-api_key    = API_KEY
-base_url   = BASE_URL
-ALPACA_CREDS = {
-    "API_KEY": api_key, 
-    "API_SECRET": api_secret, 
-    "PAPER": True
-}
-from alpaca.trading.client import TradingClient
-from alpaca.trading.requests import (MarketOrderRequest)
-from alpaca.trading.enums import (
-    OrderSide,
-    OrderType,
-    TimeInForce,
-)
-# import subprocess
-import streamlit as st
-from lumibot.backtesting.yahoo_backtesting import YahooDataBacktesting
 from lumibot.strategies import Strategy
-from lumibot.traders import Trader
-from lumibot.brokers.alpaca import Alpaca
 from .finbert_utils import estimate_sentiment
-import datetime
-from datetime import datetime 
 from alpaca_trade_api import REST 
 from datetime import timedelta 
 import os
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
-
-
-trade_client = TradingClient(api_key=api_key, secret_key=api_secret, paper=True)
-symbol = "QQQ"
-req = MarketOrderRequest(
-    symbol = symbol,
-    qty= 1 , # notional is specified in USD, here we specify $1.11
-    side = OrderSide.BUY,
-    type = OrderType.MARKET,
-    time_in_force = TimeInForce.DAY,
-)
-res = trade_client.submit_order(req)
-print(f"==========trade_client.submit_order(req) ===========:/n/n {res}")
-
 class StockSentiment(Strategy): 
     def initialize(self, symbol:str="SPY", cash_at_risk:float=.5): 
         self.symbol = symbol
@@ -108,22 +65,7 @@ class StockSentiment(Strategy):
                 self.submit_order(order) 
                 self.last_trade = "sell"
 
-start_date  = datetime(2020,1,1)
-end_date    = datetime(2020,1,12) 
-# end_date    = datetime(2023,12,31) 
-broker      = Alpaca(ALPACA_CREDS) 
-strategy    = StockSentiment(name='mlstrat', broker=broker, 
-                    parameters={"symbol":"SPY", 
-                                "cash_at_risk":.5})
-strategy.backtest(
-    YahooDataBacktesting, 
-    start_date, 
-    end_date, 
-    parameters={"symbol":"SPY", "cash_at_risk":.5}
-)
-trader = Trader()
-trader.add_strategy(strategy)
-trader.run_all()
+
 
 
 
--- t10.py
+++ t10.py
@@ -8,31 +8,70 @@ from lumibot.example_strategies.stock_diversified_leverage import DiversifiedLev
 from lumibot.credentials import IS_BACKTESTING
 from lumibot.backtesting.yahoo_backtesting import  YahooDataBacktesting
 from lumibot.example_strategies.stock_sentiment import StockSentiment
-from lumibot.credentials import ALPACA_CREDS
+
+if __name__ == "__main__":
+    backtesting_start = datetime(2023, 1, 2)
+    backtesting_end  = datetime(2024, 10, 31)
+
+    st.write(f"Backtesting: Start {backtesting_start} End {backtesting_end}")
+    IS_BACKTESTING = True
+    if not IS_BACKTESTING:
+        print("This strategy is not meant to be run live. Please set IS_BACKTESTING to True.")
+        st.write("This strategy is not meant to be run live. Please set IS_BACKTESTING to True.")        
+        exit()
+    else:
+        parameters = {
+            "market": "NYSE",
+            "sleeptime": "1D",
+            "drift_threshold": "0.05",
+            "acceptable_slippage": "0.005",  # 50 BPS
+            "fill_sleeptime": 15,
+            "target_weights": {
+                "SPY": "0.60",
+                "TLT": "0.40"
+            },
+            "shorting": False
+        }
+
+
+
+
 """
 Strategy Description
 This strategy will buy a few symbols that have 2x or 3x returns (have leverage), but will 
 also diversify and rebalance the portfolio often.
 """
 if __name__ == "__main__":
-    
-    
-    print("==================MLTrader(Strategy)=============================")
-
-    
-    start_date = datetime(2020,1,1)
-    end_date = datetime(2023,12,31) 
-    broker = Alpaca(ALPACA_CREDS) 
-    strategy = StockSentiment(name='mlstrat', broker=broker, 
-                        parameters={"symbol":"SPY", 
-                                    "cash_at_risk":.5})
-    strategy.backtest(
-        YahooDataBacktesting, 
-        start_date, 
-        end_date, 
-        parameters={"symbol":"SPY", "cash_at_risk":.5}
-    )
-    trader = Trader()
-    trader.add_strategy(strategy)
-    trader.run_all()
-    
+    is_live = False
+    if is_live:
+        ####
+        # Run the strategy live
+        ####
+        from lumibot.credentials import ALPACA_CREDS
+        trader = Trader()
+        broker = Alpaca(ALPACA_CREDS)
+        strategy = DiversifiedLeverage(broker=broker)
+        trader.add_strategy(strategy)
+        trader.run_all()
+    else:
+        ####
+        # Backtest the strategy
+        ####
+        # Choose the time from and to which you want to backtest
+        backtesting_start = datetime(2023, 6, 1)
+        backtesting_end = datetime(2023, 7, 31)
+        # 0.01% trading/slippage fee
+        trading_fee = TradingFee(percent_fee=0.005)
+        # Initialize the backtesting object
+        print("Starting Backtest...")
+        result = DiversifiedLeverage.backtest(
+            YahooDataBacktesting,
+            backtesting_start,
+            backtesting_end,
+            benchmark_asset="SPY",
+            parameters={},
+            buy_trading_fees=[trading_fee],
+            sell_trading_fees=[trading_fee],
+        )
+        print("Backtest result: ", result)
+        st.write(f"Backtest result: {result}")
